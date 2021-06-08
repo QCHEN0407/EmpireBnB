c44faca041bf8f1029c52915e10454222fdd0862
Qingwei <qchen6060@gmail.com>
2021-06-07 22:13:29 -0400

    review reducer added

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -3,11 +3,13 @@ import thunk from "redux-thunk";
 import session from "./session";
 import listings from "./listing";
 import booking from "./booking";
+import review from "./review";
 
 const rootReducer = combineReducers({
     session,
     listings,
-    booking
+    booking,
+    review
 });
 
 
