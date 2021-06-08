84365107bedb68935f596443511cedfb9d7e7f19
Qingwei <qchen6060@gmail.com>
2021-06-07 22:11:34 -0400

    review store

---
+++ react-app/src/store/review.js
@@ -0,0 +1,35 @@
+const LOAD_REVIEWS = 'review/LOAD';
+const ADD_REVIEW = 'review/ADD';
+
+const loadReviews = reviews => ({
+    type: LOAD_REVIEWS,
+    reviews
+});
+
+const addReview = review => ({
+    type: ADD_REVIEW,
+    review
+})
+
+export const getReviews = (listingId) => async(dispatch) => {
+    const res = await fetch(`/api/users/${listingId}/reviews`)
+
+    try {
+        if (!res.ok) throw res
+        const reviews = await res.json()
+        dispatch(loadReviews(reviews))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const reviewReducer = (state = initialState, action) => {
+    switch(action.type){
+        case LOAD_REVIEWS:
+            return action.reviews;
+        default:
+            return state;
+    }
+}
+
+export default reviewReducer;
